ab00054aca930ee17fd107a1554ac96ff6c8d23e
sheisuka <xxtornex@gmail.com>
2023-07-18 19:18:01 +0700

    :sparkles: Add completing to length equals power of 2, checking if sha256

--- merkle/utils.py
+++ merkle/utils.py
@@ -1,5 +1,5 @@
 from math import log2
-
+from re import match
 
 def round_to_2_power(number: int) -> int:
     """Возращает x, такой, что x это ближайшая степень 2 и x >= number. Пример: 4 -> 4, 3 -> 4, 5 -> 8"""
@@ -17,3 +17,13 @@ def padd_to_2_power(list_: list) -> list:
     for i in range(round_len - list_len):
         padded_list.append(last_element) 
     return padded_list
+
+
+def is_sha256(obj: object) -> bool:
+    """Выполняет проверку того, что входная строка - результат sha256"""
+    if isinstance(obj, str):
+        sha256_pattern = r"^[0-9a-fA-F]{64}$"
+        if match(sha256_pattern, obj):
+            return True
+        return False
+    return False
